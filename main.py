2f89284b2236f13f9df999f534117b31d08e5a1a
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-07-12 11:12:16 +0100

    csv working

---
+++ main.py
@@ -0,0 +1,11 @@
+from pdf2csv import export_tables_to_csv
+
+# Set the path to your PDF file
+pdf_path = "test.pdf"
+
+# Set the path to your template file
+template_path = "sbi_template.json"
+
+# Call the function to export the tables to CSV files
+export_tables_to_csv(pdf_path, template_path)
+
