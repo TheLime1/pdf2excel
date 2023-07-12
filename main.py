5a8116f582c5dc9900e95c7872cd8f094b2bff8f
TheLime03 <everpadd4@gmail.com>
2023-07-12 21:20:10 +0100

    test conv on gui

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from pdf2csv import *
 from csv2xl import *
 
 # Set the path to your PDF file
-pdf_path = "test.pdf"
+pdf_path = "test.pdf" #change this to the path of the prevewed pdf
 
 # Set the path to your template file
-template_path = "sbi_template.json"
+template_path = "C:\Users\everp\Documents\GitHub\pdf2excel\sbi_template.json"
 
 export_tables_to_csv(pdf_path, template_path)
 merge_csv_to_excel('.', 'output.xlsx')
