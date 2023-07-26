2a5653ab4a56e18e5c7c2e74baf01bb5963ce389
TheLime03 <everpadd4@gmail.com>
2023-07-26 13:41:32 +0100

    relative path

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from csv2xl import *
 pdf_path = "test.pdf" #change this to the path of the prevewed pdf
 
 # Set the path to your template file
-template_path = "C:\Users\everp\Documents\GitHub\pdf2excel\sbi_template.json"
+
+template_path = r"templates\sbi_template.json"
 
 export_tables_to_csv(pdf_path, template_path)
 merge_csv_to_excel('.', 'output.xlsx')
 remove_csv_files()
+print("Done")
